b418367da8246056661f5bdeb6f84597e3218497
andrestaddei <0232911@up.edu.mx>
2024-10-28 08:46:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,8 @@ st.markdown("Simulador de inversión")
 st.markdown("Selecciona uno, dos o tres ETFs para comparar su rendimiento y simular la inversión:")
 
 # Selector de periodos de tiempo
-    periodos = ['1mo', '3mo', '6mo', '1y', 'ytd', '3y', '5y', '10y']
-    seleccion_periodo = st.selectbox('Selecciona el periodo de tiempo', periodos)
+periodos = ['1mo', '3mo', '6mo', '1y', 'ytd', '3y', '5y', '10y']
+seleccion_periodo = st.selectbox('Selecciona el periodo de tiempo', periodos)
 
 
 
